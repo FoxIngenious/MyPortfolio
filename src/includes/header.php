886d552270162f9cd02f2a4d082ad454b887a3a1
FoxIngenious <jakconcamilledoringa@gmail.com>
2026-06-01 16:53:57 +0300

    headercreation

---
+++ src/includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..//css//style.css">
+</head>
+
+<body>
+    <nav class="navBar" id="mainNav">
+        <img src="..//Image//profilweb.webp" alt=" jacksonCamilleProfil" id="navProfil">
+        <ul id="menu">
+            <li>Acceuil</li>
+            <li>A propos</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+</body>
+
+</html>
