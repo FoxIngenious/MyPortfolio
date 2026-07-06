c2ef94e5f70d589bafbb4af9ec536a0d8f6c68ef
FoxIngenious <jacksoncamilledoringa.com>
2026-07-06 13:26:02 +0300

    firstCommit

--- src/includes/head.php
+++ src/includes/head.php
@@ -4,10 +4,8 @@ $page_actuelle = basename($_SERVER['PHP_SELF']);
 $page_name = pathinfo($page_actuelle, PATHINFO_FILENAME);
 ?>
 
-<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JacksonCamille | <?= $page_name === 'index' ? 'Acceuile' : $page_name ?></title> <!-- Si le nom du page = index on donne un nom Acceuille si non le nom de la page-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="/src/Js/script.js" defer></script>
-</head>
